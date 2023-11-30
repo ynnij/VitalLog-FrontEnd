b69489859b80f0970b9889bff92681906a66e62e
ynnij <ynnnij@gmail.com>
2023-11-30 18:17:19 +0900

    로그 테이블에서 로그 삭제 기능 구현

--- src/components/mypage/LogTable.js
+++ src/components/mypage/LogTable.js
@@ -1,6 +1,22 @@
+import { FiMinusCircle } from "react-icons/fi";
+
 const LogTable = ({userlog}) => {
+    console.log(userlog)
+    const deleteLog = (seq) => {
+        if(window.confirm('해당 로그를 삭제할까요?')){
+            const url = `http://10.125.121.216:8080/api/vitallog/mypage/${seq}`;
+            fetch(url, {
+                method : 'DELETE',
+                headers: {
+                    "Content-Type": 'application/json',
+                    "Authorization": localStorage.getItem("token"),
+                  }
+            }).then(resp=>window.location.reload())
+            .catch(err=>console.log(err))
+        }
+    }
     const tbodyTags = userlog && userlog.map((log, idx)=>{
-        return <tr key={log.seq} className="odd:bg-white even:bg-gray-50 border-b ">
+        return <tr key={log.seq} className="odd:bg-white even:bg-gray-50 border-b">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
                             {log.exerDate}
                         </th>
@@ -11,7 +27,10 @@ const LogTable = ({userlog}) => {
                             {log.exercise}
                         </td>
                         <td className="px-6 py-4">
-                            {log.kcal} kcal
+                            {log.kcal} kcal  
+                        </td>
+                        <td className="pr-6 pt-5 h-full flex justify-end">
+                            <FiMinusCircle onClick={()=>deleteLog(log.seq)} className="hover:text-[#D4573A] hover:cursor-pointer" />
                         </td>
                         
                     </tr>
@@ -34,7 +53,8 @@ const LogTable = ({userlog}) => {
                         <th scope="col" className="px-6 py-3">
                             kcal
                         </th>
-
+                        <td className=""> 
+                        </td>
                     </tr>
                 </thead>
                 <tbody>
